4518313122e5ae34d8fb229fef945780c168095b
Chickensoup269 <thientran01345@icloud.com>
2025-04-02 18:47:54 +0700

    V8.4 - Accept all current changes. v2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,64 +1,33 @@
-import express from "express";
-<<<<<<< HEAD
-import cors from "cors";
-import dotenv from "dotenv";
-import setupRoutes from './routes/appRoutes'
-import morgan from 'morgan';
-import { connectDB } from "./config/db";
-import { fetchMoviesFromTMDB } from "./services/fetchmovieServices";
-import { fetchAndStoreGenres } from "./services/fetchgenresServices";
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import setupRoutes from "./routes/appRoutes"
+import morgan from "morgan"
+import { connectDB } from "./config/db"
+import { fetchMoviesFromTMDB } from "./services/fetchmovieServices"
+import { fetchAndStoreGenres } from "./services/fetchgenresServices"
 // import {seedCinemas} from "./services/seedData"
-import { startCronJobs } from './utils/cronUtils';
+import { startCronJobs } from "./utils/cronUtils"
 
+dotenv.config()
 
-=======
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import movieRoutes from "./routes/movies/movies.routes";
-import authRoutes from "./routes/auth/auth.routes";
-import usersRoutes from "./routes/users/users.routes";
-import movieCreditsRoutes from "./routes/movies/movieCredits.routes";
->>>>>>> main
+const app = express()
+const PORT = 3001
 
-dotenv.config();
-
-const app = express();
-<<<<<<< HEAD
-const PORT = 3001;
-
-app.use(cors());
-=======
->>>>>>> main
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 app.use(morgan("combined"))
 
-<<<<<<< HEAD
 connectDB().then(async () => {
-  console.log("Kết nối MongoDB thành công!");
-  await fetchMoviesFromTMDB();
-  await fetchAndStoreGenres();
+  console.log("Kết nối MongoDB thành công!")
+  await fetchMoviesFromTMDB()
+  await fetchAndStoreGenres()
   // await seedCinemas();
-  startCronJobs();
-});
+  startCronJobs()
+})
 
-setupRoutes(app);
+setupRoutes(app)
 // Start server
-=======
-// Kết nối MongoDB Atlas
-mongoose
-  .connect(process.env.MONGO_URI as string)
-  .then(() => console.log("Kết nối MongoDB Atlas thành công"))
-  .catch((err) => console.error("Lỗi kết nối MongoDB Atlas:", err));
-
-// Route
-app.use("/api/movies", movieRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/users", usersRoutes);
-app.use("/api/movies", movieCreditsRoutes);
-
-// PORT
-const PORT = process.env.PORT || 5000;
->>>>>>> main
 app.listen(PORT, () => {
-  console.log(`Server chạy tại http://localhost:${PORT}`);
-});
+  console.log(`Server chạy tại http://localhost:${PORT}`)
+})
