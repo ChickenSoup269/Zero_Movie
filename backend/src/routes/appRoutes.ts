ae2be9acb4eb51d77141735f40d4db46148dd396
Chickensoup269 <thientran01345@icloud.com>
2025-04-11 19:26:37 +0700

    V11.2 - v2

--- backend/src/routes/appRoutes.ts
+++ backend/src/routes/appRoutes.ts
@@ -1,29 +1,29 @@
-import { Application } from "express";
-import moviesRoutes from "./moviesRoutes";
-import genresRoutes from "./genresRoutes";
-import cinemaRoutes from "./cinemaRoutes";
-import roomRoutes from "./roomRoutes";
-import seatRoutes from "./seatRoutes";
-import showtimeRoutes from "./showtimeRoutes";
-import showtimeseatRoutes from "./showtimeseatRoutes";
-import userRoutes from "./userRoutes";
-import authRoutes from "./authRoutes";
-import bookingRoutes from "./bookingRoutes";
-import paymentRoutes from "./paymentRoutes";
-import paymentRouter from "./paymentRouter";
+import { Application } from "express"
+import moviesRoutes from "./moviesRoutes"
+import genresRoutes from "./genresRoutes"
+import cinemaRoutes from "./cinemaRoutes"
+import roomRoutes from "./roomRoutes"
+import seatRoutes from "./seatRoutes"
+import showtimeRoutes from "./showtimeRoutes"
+import showtimeseatRoutes from "./showtimeseatRoutes"
+import userRoutes from "./userRoutes"
+import authRoutes from "./authRoutes"
+import bookingRoutes from "./bookingRoutes"
+import paymentRoutes from "./paymentRoutes"
+import paymentRouter from "./paymentRoutes"
 
 export default function setupRoutes(app: Application) {
-  app.use("/api/auth", authRoutes);
-  app.use("/api/users", userRoutes);
-  app.use("/api/bookings", bookingRoutes);
-  app.use("/api/payment", paymentRouter);
+  app.use("/api/auth", authRoutes)
+  app.use("/api/users", userRoutes)
+  app.use("/api/bookings", bookingRoutes)
+  app.use("/api/payment", paymentRouter)
 
-  app.use("/api/payments", paymentRoutes);
-  app.use("/api/cinemas", cinemaRoutes);
-  app.use("/api/movies", moviesRoutes);
-  app.use("/api/genres", genresRoutes);
-  app.use("/api/room", roomRoutes);
-  app.use("/api/seats", seatRoutes);
-  app.use("/api/showtime", showtimeRoutes);
-  app.use("/api/showtimeseat", showtimeseatRoutes);
+  app.use("/api/payments", paymentRoutes)
+  app.use("/api/cinemas", cinemaRoutes)
+  app.use("/api/movies", moviesRoutes)
+  app.use("/api/genres", genresRoutes)
+  app.use("/api/room", roomRoutes)
+  app.use("/api/seats", seatRoutes)
+  app.use("/api/showtime", showtimeRoutes)
+  app.use("/api/showtimeseat", showtimeseatRoutes)
 }
