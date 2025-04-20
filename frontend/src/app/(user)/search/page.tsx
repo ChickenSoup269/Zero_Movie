2c9129d09c91e86f90e1a523fe269f692bb2885c
Chickensoup269 <thientran01345@icloud.com>
2025-04-21 01:07:59 +0700

    V14.6 - sửa trang movies tìm kiếm

--- frontend/src/app/(user)/search/page.tsx
+++
@@ -1,9 +0,0 @@
-import ChatBox from "@/components/ui-chatBox/chat-box"
-
-export default function SearchPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center ">
-      <ChatBox />
-    </div>
-  )
-}
---
+++ frontend/src/app/(user)/testpage/page.tsx
@@ -0,0 +1,3 @@
+export default function testPage() {
+  return <div className="min-h-screen flex items-center justify-center "></div>
+}
